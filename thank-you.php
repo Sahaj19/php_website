09669a6f50718ba3f5c9723cf8fa68268daaa04b
SahajArora <sahajarora1906@gmail.com>
2024-03-23 03:09:51 +0530

    typo fix

--- thank-you.php
+++ thank-you.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <div class="thank_you_div">
-        <h1>Thank you, Your form have been submitted successfully!</h1>
+        <h1>Thank you, Your form has been submitted successfully!</h1>
         <a href="show-records.php">Show all Records</a>
     </div>
 </body>
